18e4781a8fe1f7cbabb578b8ec9f45422348cbf7
Demoleas715 <Demoleas715@gmail.com>
2017-03-24 21:22:31 -0400

    Login page created

--- blog.py
+++ blog.py
@@ -6,6 +6,9 @@ import sqlite3
 
 # configuration
 DATABASE = "blog.db"
+USERNAME = 'admin'
+PASSWORD = 'admin'
+SECRET_KEY = '\x12j\xad\x9a\xa3\x9f\xfbmX\xab{L\xaaic\xce\xed\xf7q\xddR\xb4!\xd2'
 
 app = Flask(__name__)
 
@@ -16,13 +19,29 @@ app.config.from_object(__name__)
 def connect_db():
     return sqlite3.connect(app.config['DATABASE'])
 
-@app.route('/')
+@app.route('/', methods = ['GET', 'POST'])
 def login():
-    return render_template('login.html')
+    error = None
+    if request.method == 'POST':
+        if request.form['username'] != app.config['USERNAME'] or request.form['password'] != app.config['PASSWORD']:
+            error = 'Invalid Credentials. Please try again.'
+        else:
+            session['logged_in'] = True
+            return redirect(url_for('main'))
+    return render_template('login.html', error = error)
+
+@app.route('/logout')
+def logout():
+    session.pop('logged_in', None)
+    flash('You were logged out')
+    return redirect(url_for('login'))
 
 @app.route('/main')
 def main():
     return render_template('main.html')
+
+
+
 
 if __name__ == "__main__":
     app.run(debug = True)\
